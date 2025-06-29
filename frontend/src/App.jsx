751a3df94c6c5bc262cf066de313d53e8e6ad0a6
Aniket793 <vinitkshirsagar18@gmail.com>
2025-06-29 18:50:12 +0530

    Added routing setup.
    react-router-dom to connect and route pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,20 @@
 
 import React from 'react';
 import HomePage from './components/pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LinkedInLinkInput from './components/pages/LinkedInLinkInput';
+import NavBar from './components/atoms/NavBar/NavBar';
 
 function App() {
-  return <HomePage />;
+   return (
+     <Router>
+      <NavBar /> {/* ✅ Now inside Router */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/linkedin" element={<LinkedInLinkInput />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
